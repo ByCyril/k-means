36b06e4916dca1b1138b8f9e3e0ab9c0fb0a6028
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2018-04-10 23:00:36 -0700

    mvp done

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 
 var width = 30
 var height = 30
-var idArray = [];
+
+var groupA = [];
+var groupB = [];
+var flag = 0;
 
 function init() {
 	createGrid();
@@ -17,14 +20,104 @@ function makePoint() {
 }
 
 function getAllPoints() {
+
+	var idArray = [];
+
 	$('.point').each(function () {
 	    idArray.push(this.id);
 	});
 
-	for (var i = 0; i < idArray.length; i++) {
-		print(idArray[i]);
-	}
+	setRandomPoints(idArray);
+
+	idArray = [];
+
 }
+
+function set(x, y, value) {
+	document.getElementById(x + "-" + y).setAttribute("class", value);
+}
+
+
+
+function setRandomPoints(points) {
+
+	var groupATemp = [];
+	var groupBTemp = [];
+
+	var x1 = rand(2, width - 2);
+	var y1 = rand(2, height - 2);
+	var x2 = rand(2, width - 2);
+	var y2 = rand(2, height - 2);
+
+	set(x1, y1, "testPoint");
+	set(x2, y2, "testPoint");
+
+	var slope =  Math.abs((y2 - y1) / (x2 - x1));
+
+	var midpointX = (x2 + x1) / 2;
+	var midpointY = (y2 + y1) / 2;
+
+
+	var yIntercept = midpointY - (midpointX * slope);
+
+	for (var i = 0; i < points.length; i++) {
+		var x = points[i].split("-")[0];
+		var y = points[i].split("-")[1];
+
+		var yl = (slope * x) + yIntercept;
+
+		var dy = yl - y;
+
+		if (dy > 0 && slope > 0) {
+			set(x, y, "groupA");
+			groupATemp.push(points[i]);
+		} else if (dy > 0 && slope < 0) {
+			set(x, y, "groupA");
+			groupATemp.push(points[i]);
+		} else if (dy < 0 && slope > 0) {
+			set(x, y, "groupB");
+			groupBTemp.push(points[i]);
+		} else if (dy < 0 && slope < 0) {
+			set(x, y, "groupB");
+			groupBTemp.push(points[i]);
+		}
+	}
+
+	print("aTemp" + ": " +  groupATemp);
+	print("A" + ": " +  groupA);
+	
+	print("bTemp" + ": " +  groupBTemp);
+	print("B" + ": " +  groupB);
+	
+	print(groupATemp.length == groupA.length);
+	print(groupBTemp.length == groupB.length);
+
+	print("");
+
+	if ((groupATemp.length == groupA.length && flag != 0)) {
+		print("yay");
+
+
+	} else {
+		
+		flag = flag + 2;
+
+		print("Nope");
+
+		groupA = groupATemp;
+		groupB = groupBTemp;
+
+		set(x1, y1, "grid");
+		set(x2, y2, "grid");
+
+		setRandomPoints(points);
+
+	}
+
+
+
+}
+
 
 function createGrid() {
 	document.write("<table>");
@@ -40,6 +133,10 @@ function createGrid() {
 	document.write("</table>");
 }
 
+function rand(min, max) {
+	return Math.floor(Math.random() * (max - min) + min)
+}
+
 function print(x) {
 	console.log(x);
 }
@@ -53,6 +150,8 @@ $(document).ready(function() {
 		if (event.which == 13) {
 			getAllPoints();
 		}
+
+
 	});
 
 })
